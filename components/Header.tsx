abcc12be870b9f6ba67b0ba8c232b132998a174d
Biron <bironbenoit85@gmail.com>
2023-03-13 16:46:05 +0100

    second push

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,45 @@
+"use client"
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
+import { FormEvent, useRef } from "react"
 
 
 function Header() {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const input = inputRef.current?.value
+        if (!input) return
+
+        if (inputRef.current.value) {
+            inputRef.current.value = ''
+        }
+
+        try {
+            // Call our API to activate the scraper...
+            // /api/activatescraper
+        } catch (error) {
+            // Handle any errors
+        }
+
+
+        // Wait for the response to come back
+
+    }
+
+
+
   return (
     <header className="flex" >
-        <form className="flex items-center space-x-2 justify-center rounded-full py-2 px-4 bg-indigo-100 max-w-md mx-auto">
-            <input type="text"  placeholder="Search..." className="flex-1 outline-none bg-transparent text"/>
+        <form className="flex items-center space-x-2 justify-center rounded-full py-2 px-4 bg-indigo-100 max-w-md mx-auto" onSubmit={ handleSearch}>
+
+            <input type="text"  placeholder="Search..." className="flex-1 outline-none bg-transparent text-indigo-300 placeholder:text-indigo-300" ref={inputRef}/>
+
             <button hidden>Search</button>
             <MagnifyingGlassIcon className="h-6 w-6 text-indigo-300"/>
+        
         </form>
     </header>
   )
